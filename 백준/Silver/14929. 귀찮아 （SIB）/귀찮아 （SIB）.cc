b5a25f35ba78d64896ad48276df9264edd400c9c
kmk4729 <53010625+kmk4729@users.noreply.github.com>
2025-04-08 20:28:14 +0900

    [Silver V] Title: 귀찮아 (SIB), Time: 32 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14929. 귀찮아 （SIB）/귀찮아 （SIB）.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+
+int main() {
+
+	int N;
+	cin >> N;
+	int minisum = 0;
+	long sumd = 0;
+	int num;
+	cin >> num;
+	minisum = num;
+	for (int i = 1; i < N; i++)
+	{
+		cin >> num;
+		sumd += minisum * num;
+		minisum += num;
+
+	}
+	
+	
+	cout << sumd;
+}
