0ea244e4a141d85341f8aa64f2b06a9e3db7fe6b
kmk4729 <53010625+kmk4729@users.noreply.github.com>
2025-05-11 18:47:19 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(){
+	int num, a, b;
+	cin >> num;
+	for (int i = 0; i < num; i++)
+	{
+		cin >> a >> b;
+		cout << "yes\n";
+	}
+}
